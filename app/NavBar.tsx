9bd3526d1257b3a69896cbec745042f4a2ca6815
oreolion <remyoreo11@gmail.com>
2024-05-21 06:40:58 -0700

    style navbar active link;

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,23 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
   const links: any = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-        <FaBug />
+      <FaBug />
       <ul className="flex space-x-6 ">
         {links.map((link: any) => {
-          return <Link
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-            href={link.href}
-            key={link.href}
-          >
-            {link.label}{" "}
-          </Link>;
+          return (
+            <Link
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors":
+                  link.href !== currentPath,
+              })}
+              href={link.href}
+              key={link.href}
+            >
+              {link.label}{" "}
+            </Link>
+          );
         })}
       </ul>
     </nav>
